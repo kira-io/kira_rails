85bdfcb3eebd7ecc45d75baa22af9577ff41295d
Andrew Lee <andyrewlee@gmail.com>
2014-10-16 01:52:29 -0700

    Add email and password tests to user, start entry model specs

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Entry, :type => :model do
+  before do
+    @user = User.create(:alias => 'kobe', :password => 'password', :password_confirmation => 'password')
+    @entry = Entry.new(:title => 'My Amazing Day',
+                       :content => 'Something happened and I laughed. I laughed again',
+                       :user => @user)
+  end
+
+  it 'should be valid with title, content, and user' do
+    expect(@entry).to be_valid
+  end
+
+  describe 'an entry without a title' do
+    it 'should be invalid with no title' do
+      @entry.title = ' '
+      expect(@entry).to_not be_valid
+    end
+
+    it 'should be invalid with too short of title' do
+      @entry.title = 'x'
+      expect(@entry).to_not be_valid
+    end
+  end
+
+  describe 'an entry without proper amount of content' do
+    it 'should be invalid with no content'
+    it 'should be invalid with too short of content'
+  end
+
+  describe 'an entry without user' do
+    it 'should be invalid without user reference'
+  end
 end
