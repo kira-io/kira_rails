1ae22696ce6f3999bb792d995ee483ddb628cb83
Andrew Lee <andyrewlee@gmail.com>
2014-10-18 22:52:06 -0700

    Fix socket, turbolinks in chatroom

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,58 +1,38 @@
-var socket = io.connect("http://192.168.15.202:7777", {force_connection: true});
+var socket = io.connect("http://192.168.15.127:7777", {force_connection: true});
 
-$(document).ready(function(){
 
-	var room_number = null;
-	var user_name = null;
-	// console.log('user-name', user_name)
-	
-	//user clicks on room name to join a room
-	$(document).on('click', '.post_title', function(){
-		console.log($(this).attr('id')); //grab room id #
-		
-		// user_name = $('#user_name').text();
-		// console.log('user_name', user_name);
+var room_number =0;
+var user_name = 'kira';
 
-		if(room_number == null){
-			room_number = $(this).attr('id');
-			socket.emit('join_room', {room: room_number})
-		} else if(room_number != null){
-			old_room = room_number
-			room_number = $(this).attr('id');
-			socket.emit('change_room', {prev_room: old_room, new_room: room_number})
-		}
-	});
-	
-	//listening for broadcast to room - test broadcast
-	socket.on('get_name', function(data){
-		user_name = $('#user_name').text();
-		// console.log('user_name', user_name);
-		socket.emit('user_name', {name: user_name})
-		// console.log("data.message", data.name, data.message);
-	});
+$(document).on('click', '.post_title', function(){
+    console.log($(this).attr('id')); //grab room id #
 
-	socket.on('name', function(data){
-		user_name = data.name
-	})
- 	
- 	//emit message to chatroom
-	$(document).on('submit', 'form', function(){
-		console.log($('#message').val().replace(/(<([^>]+)>)/ig,""));
-		var message = $('#message').val().replace(/(<([^>]+)>)/ig,"");
+    user_name = $('#user_name').text();
+    if(room_number == 0){
+        room_number = $(this).attr('id');
+        console.log('I WANT TO JOIN THIS ROOM', room_number);
+        socket.emit('client:join_room', {room: room_number});
+    } else {
+        old_room = room_number;
+        room_number = $(this).attr('id');
+        socket.emit('client:change_room', {prev_room: old_room, new_room: room_number});
+    }
+});
 
-		if(message.replace(/\s/g,"") == ""){
-			//if string is empty or spaces do nothing
-		} else{
-			socket.emit('emit_message', {room: room_number, name: user_name, message: message});
-		}
-		$('#message').val('');
-		return false;
-	});
 
-	//append incoming message
-	socket.on('incoming_message', function(data){
-		// console.log("INCOMING MESSAGE", data.name, data.message);
-		$('#chat_room').append("<p>" + data.name + ": " + data.message + "</p>");
-	});
 
+$(document).on('submit', 'form', function(){
+    console.log($('#message').val().replace(/(<([^>]+)>)/ig,""));
+    var message = $('#message').val().replace(/(<([^>]+)>)/ig,"");
+    room_number = $(this).attr('id');
+
+    socket.emit('client:emit_message', {room: room_number, name: user_name, message: message});
+
+    $('#message').val('');
+    return false;
+});
+
+socket.on('server:incoming_message', function(data){
+    console.log("INCOMING MESSAGE", data.name, data.message);
+    $('#chat_room').append("<p>" + data.name + ": " + data.message + "</p>");
 });
