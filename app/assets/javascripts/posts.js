795d6074c2f2cbe96c16eb047cb768dcb86ab1b8
Eduardo <edubaik@gmail.com>
2014-10-21 01:39:02 -0700

    Fixed chat alias

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,14 +1,24 @@
-var socket = io.connect("http://192.168.15.170:7777", {force_connection: true});
+var socket = io.connect("http://192.168.15.228:7777", {force_connection: true});
 
 var room_number = 0;
 var user_name = 'kira';
+$(document).ready(function(){
+  user_name = $('#user_name').text();
+  socket.emit('client:send_user_name', {name: user_name})
+
+  socket.on('server:user_name', function(data){
+    user_name = data.name
+  }); 
+});
 
 $(document).on('submit', 'form', function(){
+  
   console.log($('#message').val().replace(/(<([^>]+)>)/ig,""));
   var message = $('#message').val().replace(/(<([^>]+)>)/ig,"");
   room_number = $(this).attr('id');
 
-  socket.emit('client:emit_message', {room: room_number, name: user_name, message: message});
+  // socket.emit('client:emit_message', {room: room_number, name: user_name, message: message});
+  socket.emit('client:emit_message', {room: room_number, message: message});
 
   $('#message').val('');
   return false;
@@ -23,6 +33,7 @@ socket.on('server:expired_room', function (data){
 
 socket.on('server:incoming_message', function(data){
   console.log("INCOMING MESSAGE", data.name, data.message);
-  $('#chat_room').append("<p>" + data.name + ": " + data.message + "</p>");
+  // $('#chat_room').append("<p>" + data.name + ": " + data.message + "</p>");
+  $('#chat_room').append("<p>" + user_name + ": " + data.message + "</p>");
 });
 
